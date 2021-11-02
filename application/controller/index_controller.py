4434c001b9dc76d5c9d3e1a5cc6f3aa8e45a7fb0
gabsgc <gabriellagarccia@gmail.com>
2021-11-02 16:52:49 -0300

    initial settings + create index, login, register

---
+++ application/controller/index_controller.py
@@ -0,0 +1,18 @@
+from application import app
+from flask import render_template
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
